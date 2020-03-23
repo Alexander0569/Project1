d96b00d4c8fe6ebaf5081eee3542c3f730bf40a0
Александр Смирнов <alexanbsmirnov@gmail.com>
2020-03-23 17:56:05 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+
+            Console.WriteLine("Результат");
+            Console.WriteLine(a);
+
+            Console.ReadLine();
+        }
+    }
+}
